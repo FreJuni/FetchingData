efb782fa245cffcd8f16f4c3974ebe341c5c9a85
FreJuni <websitepsw11@gmail.com>
2023-12-10 12:40:00 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import { useState } from "react";
-import ShowMem from "./components/SowMem";
-import Form from "./components/Form";
+import React, { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+
+const url = "https://jsonplaceholder.typicode.com/todos";
 
 function App() {
-  const [info, setInfo] = useState([]);
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState(0);
 
-  const getUserInfo = (prop) => {
-    setInfo([...info, prop]);
+  const fetchData = async (URL) => {
+    const response = await fetch(URL);
+    const Data = await response.json();
+    setData([...data, Data]);
+  };
+
+  // useEffect(() => {
+  //   fetchData(url);
+  // }, []);
+
+  console.log(data);
+
+  const handle = (e) => {
+    e.preventDefault();
+
+    if (value < 1 || value > 200) {
+      alert("You enter worong number sorry.");
+      return;
+    }
+
+    fetchData(`${url}/${value}`);
+    setValue(0);
+    console.log("helo");
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-container">
-          <Form getUserInfo={getUserInfo} />
-          {info.length === 0 ? (
-            <p className="no-user">There was no user in your app.</p>
-          ) : (
-            info.map((item, index) => {
-              return <ShowMem key={index} item={item} />;
-            })
-          )}
+    <section className="section-center">
+      <div className="section-con">
+        <h2>Text here number between 1 to 200.</h2>
+        <form onSubmit={handle}>
+          <input
+            type="number"
+            name="number"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          ></input>
+          <button className="btn">Enter </button>
+        </form>
+        <div className="testing-con">
+          {data &&
+            data.map((item, index) => {
+              return (
+                <div key={item.id} className="item-con">
+                  <h1>Produces : {index + 1}</h1>
+                  <h3>Title : {item.title}</h3>
+                  <p>Title ID : {item.id}</p>
+                </div>
+              );
+            })}
         </div>
-      </header>
-    </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/Carts.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Children } from 'react'
-
-const Carts = (props) => {
-  return (
-    <div className={`${props.css} cart-con`}>{props.children}</div>
-  )
-}
-
-export default Carts
